1ccee37a6880a90abfeb48619037057dc62a2359
psyomn <lethaljellybean@gmail.com>
2013-05-17 15:51:40 -0400

    Minor code cleanup. Add the transaction handlers.

--- axios/src/request_helpers.adb
+++ axios/src/request_helpers.adb
@@ -5,26 +5,20 @@ package body Request_Helpers is
   -- is and return the valid enumaration).
   function Parse_Request_Type(R : String)
   return Request_Type is 
-    package tmp renames Ada.Text_IO;
     First, Last : Positive;
     Found       : Boolean;
   begin
     Get_Word(R, First, Last, Found);
 
     if    R(First..Last) = "GET"    then
-      tmp.put_line("Get request");
       return GET;
     elsif R(First..Last) = "POST"   then 
-      tmp.put_line("post request");
       return POST;
     elsif R(First..Last) = "PUT"    then
-      tmp.put_line("put request");
       return PUT;
     elsif R(First..Last) = "HEAD"   then 
-      tmp.put_line("head request");
       return HEAD;
     elsif R(First..Last) = "DELETE" then
-      tmp.put_line("delete request");
       return DELETE;
     else
       return GET;
@@ -38,23 +32,25 @@ package body Request_Helpers is
   --   ...
   -- 
   -- Would return a string with "index.html"
-  -- @note Thanks to
-  --   http://rosettacode.org/wiki/Regular_expressions#Ada
   function Parse_Request_URI(R : String)
   return String is 
     First, Last : Positive;
     Found : Boolean;
-    -- Breaking everything down to words will work fine. 
   begin 
+    -- Called twice, because the second 'word' is the uri in the request
+    -- header.
     Get_Word(R, First, Last, Found);
     Get_Word(R(Last+1..R'Last), First, Last, Found);
 
     if Found then
-    return R(First..Last);
+      return R(First..Last);
     end if;
+
     return "uri not found.";
   end Parse_Request_URI;
 
+  -- @note Thanks to
+  --   http://rosettacode.org/wiki/Regular_expressions#Ada
   procedure Get_Word(S           : String; 
                      First, Last : out Positive; 
 		     Found       : out Boolean) is
