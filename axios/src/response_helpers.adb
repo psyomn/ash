dc9970b5f036bf82eb55621b73ebb593b8739486
psyomn <lethaljellybean@gmail.com>
2013-07-01 22:33:43 -0400

    Make date method for response helpers actually return something.

--- axios/src/response_helpers.adb
+++ axios/src/response_helpers.adb
@@ -1,3 +1,5 @@
+with Ada.Calendar;
+with GNAT.Calendar.Time_IO;
 
 package body Response_Helpers is 
 
@@ -15,10 +17,20 @@ package body Response_Helpers is
 
   function Response_Date
   return String is
+    package ac  renames Ada.Calendar;
+    package gct renames GNAT.Calendar.Time_IO;
+    Current_Time : ac.Time;
+    Format       : gct.Picture_String := "%a, %d %B %Y %H:%M:%S EST";
+    Field_Name   : constant String    := "Date: ";
   begin
-    return "asd";
+    Current_Time := ac.Clock;
+    return 
+      Field_Name &
+      gct.Image(Current_Time, Format);
   end Response_Date;
 
+  -- Make the response for the browser. This function includes the headers,
+  -- (including the content-length field of the body), and the message body.
   function Make_Response(S : String)
   return String is
   begin
