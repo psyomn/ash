6c1b9b721371559d67f5c658124edc9390e7df43
Simon Symeonidis <lethaljellybean@gmail.com>
2017-03-23 23:43:50 -0400

    Make transaction handlers return status codes

--- src/transaction_handlers.adb
+++ src/transaction_handlers.adb
@@ -1,4 +1,6 @@
 with File_Utils;
+
+with HTTP_Status; use HTTP_Status;
 with Request_Helpers, Response_Helpers;
 use Request_Helpers, Response_Helpers;
 
@@ -10,17 +12,20 @@ package body Transaction_Handlers is
       case R_Type is
          when GET =>
             --  TODO Sanitize URLs
-            return Make_Response (File_Utils.Read (Context & "/" & URI));
+            return Make_Response (OK, File_Utils.Read (Context & "/" & URI));
          when POST =>
-            return Make_Response ("<h1>Got Post</h1>");
+            return Make_Response (OK, "<h1>Got Post</h1>");
          when PUT =>
-            return Make_Response ("<h1>Got Put</h1>");
+            return Make_Response (OK, "<h1>Got Put</h1>");
          when DELETE =>
-            return Make_Response ("<h1>Got DELETE</h1>");
+            return Make_Response (OK, "<h1>Got DELETE</h1>");
          when HEAD =>
-            return Make_Response ("<h1>Got HEAD</h1>");
+            return Make_Response (OK, "<h1>Got HEAD</h1>");
          when others =>
-            return Make_Response ("<h1>Error!</h1>");
+            return Make_Response (INTERNAL_ERROR, "<h1>Error!</h1>");
       end case;
+   exception
+      --  TODO: This should catch ENOENT instead
+      when others => return Make_Response (NOT_FOUND, "Resource not found");
    end Handle_Request;
 end Transaction_Handlers;
